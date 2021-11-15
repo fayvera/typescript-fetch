c02596e1eb3a7ec42e50b24c09b48b0771e11248
Fay Vera <fayverawright@gmail.com>
2021-11-14 23:32:55 -0500

    adds type of data to variables:

--- index.ts
+++ index.ts
@@ -2,6 +2,28 @@ import axios from 'axios';
 
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 axios.get(url).then(response => {
-  console.log(response.data)
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  
+  logTodo(id, title, completed);
+
 }) ;
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The id of this todo item is ${id}
+  The title is ${title}
+  Is it completed? ${completed}
+  `)
+};
+// ts-node index.ts
+// this command combines "tsc index.ts" & "node index.js"

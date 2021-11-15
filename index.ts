1d54f5c552a0c06410886411d25faa52019b5e3a
Fay Vera <fayverawright@gmail.com>
2021-11-15 12:07:40 -0500

    learns syntax for functions

--- index.ts
+++ index.ts
@@ -45,6 +45,16 @@ let point: {x: number; y: number} = {
 }
 
 
+// Function
+// we want to tell typescript what arguments the function will take and what it will return
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+}
+
+//" (i:number) => void" is the annotation, it is telling typescript that it will receive a number i as an argument
+// and return nothing, so use the keyword "void"
+
+
 
 // ts-node index.ts
 // this command combines "tsc index.ts" & "node index.js"

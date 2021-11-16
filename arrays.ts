61df47d45d5016447aab66ea4711df5708b9529c
Fay Vera <fayverawright@gmail.com>
2021-11-15 20:25:22 -0500

    declares arrays

---
+++ arrays.ts
@@ -0,0 +1,13 @@
+// Typed Array
+
+const carMakers = ["ford", "toyota", "chevy"]
+
+const dates = [new Date(), new Date()]
+
+const carsByMake: string[][] = []
+
+// We write the annotations when we don't initialize the constant with values
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
